e63ecdd66a814ee2c159903901890fe9df249781
Shayekasultana <114674580+Shayekasultana@users.noreply.github.com>
2022-10-08 10:10:15 +0600

    Add files via upload

--- LAB_TASK_1_PROBLEM_3(22-47308-1).cpp
+++ LAB_TASK_1_PROBLEM_3(22-47308-1).cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+void func(int a,int b)
+{
+     int x;
+    cin>>x;
+    int arr[x];
+    for(int i=0;i<x ;i++)
+    {
+        cin>>arr[i];
+    }
+
+
+
+   cout<<endl;
+    for(int j=0;j<x;j++)
+    {
+
+       for(int i=a;i<=b;i++)
+        {
+        if(i%2!=0&& arr[j]==i)
+        cout<<i<<" ";
+        }
+    }
+}
+int main()
+{
+   int a,n;
+    cin>>a>>n;
+
+
+    func(a,n);
+}
+
 
